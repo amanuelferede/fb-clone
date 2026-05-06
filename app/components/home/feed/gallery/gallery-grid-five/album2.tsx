54d8a32d8a38eca91c932f5b0b49789d474a198a
unknown <amanuelfrc@gmail.com>
2026-05-06 15:29:11 +0300

    refactored-code

--- app/components/home/feed/gallery/gallery-grid-five/album2.tsx
+++ app/components/home/feed/gallery/gallery-grid-five/album2.tsx
@@ -11,10 +11,10 @@ export default function Album2() {
       <div className="grid grid-rows-12 gap-1 h-full">
         <div className="row-span-7">
           <div className="grid grid-cols-2">
-            {medias.slice(0, 2).map((media) => (
+            {medias.slice(0, 2).map((media, index) => (
               <div
                 key={media.id}
-                className="w-full h-full"
+                className={`w-full h-full relative  ${index === 1 && postType === "share" ? "rounded-tr-2xl" : ""}`}
                 style={{
                   backgroundImage: "url(" + `${media.url}` + ")",
                   backgroundPosition: "top center",
