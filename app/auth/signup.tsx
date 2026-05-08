0dcea6e9c1bfc48c765a00f687fe80b73833dd59
unknown <amanuelfrc@gmail.com>
2026-05-08 15:36:07 +0300

    refactored-code

---
+++ app/auth/signup.tsx
@@ -0,0 +1,12 @@
+"use client";
+export default function SignUp() {
+  return (
+    <div className="max-w-lg mx-auto">
+      <p className="text-3xl font-semibold">Get started with Facebook</p>
+      <p className="text-lg">
+        Create an account to connect with friends, family and communities of
+        people who share your intersts.
+      </p>
+    </div>
+  );
+}
